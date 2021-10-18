955a3c9f417f92edafc9cd6ff68c0e79107ffddb
JigSaW <misho_1998@mail.ru>
2021-10-18 15:12:54 +0300

    created a model of participants

---
+++ meeting_site/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+GENDER_CHOICES = (
+	('M', 'Male'),
+	('F', 'Female'),
+)
+
+class Participant(models.Model):
+	photo = models.ImageField(upload_to='static/photos')
+	gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	email = models.EmailField(max_length=254)
+
+	def __str__(self):
+		return f"{self.first_name} {self.last_name}"
+
+	class Meta:
+		verbose_name = 'Участник'
+		verbose_name_plural = 'Участники'
+
